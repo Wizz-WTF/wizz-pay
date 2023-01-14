51b406d14b6d27929d2708a1dab2bf4db7bd24fc
0xJiro <jiro@sushi.com>
2023-01-14 13:54:03 -0500

    replaced sample contract w/ wizzpay

---
+++ src/WizzPay.sol
@@ -0,0 +1,14 @@
+/// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.0;
+
+contract WizzPay {
+    // functions to setup different style of nft payment streams
+
+    // a) NFTX - drop nft in pool and stream the tokens that come out
+
+    // b) Swap ETH - to nftx tokens and start streaming those
+
+    // c) specific NFT - let's store it in this contract and drop a lock on it until the stream has ended
+
+
+}
